8a6799a255c8e7e207341208317f401681b40264
chromico <atech5122@gmail.com>
2021-03-19 16:22:21 +0400

    Audiowide font added

---
+++ typeface/Audiowide-Regular.py
@@ -0,0 +1,20 @@
+###########################################
+# Glyph position table for buzzard.py
+###########################################
+# TYPEFACE: Audiowide-Regular
+# All glyphs' y-min is set on the baseline
+# All glyphs' x-pos is monospace default
+# Entries in this table are complex numbers
+# where the real component is added to the 
+# x position of the glyph and the imaginary
+# component is added to the y position 
+###########################################
+
+glyphPos = {
+    "g": 0+25j, "j": 15+25j, "p": 0+25j, "q": 0+20j, "y": 0+25j, "-": 0-55j, "*": 0-65j, 
+    "#": 0+0j, "$": 0+15j, "^": 0-90j, "~": 0-90j, "+": 0-25j, "=": 0-35j, "?": 0+0j, 
+    ";": 0+25j, ",": 0+25j, "'": 0-80j, "\"": 0+0j, "(": 0+30j, ")": 0+30j, "<": 0+0j, ">": 0+0j,
+    "{": 0+30j, "}": 0+30j, "[": 0+30j, "]":0+30j, "|": 0+30j, "\\": 0+0j, "/": 0+0j, "²": 0-60j
+}
+
+spaceDistance = 50
